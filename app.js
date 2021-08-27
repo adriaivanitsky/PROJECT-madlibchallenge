d1e4fca4769cd8463dcb010238c9c925f66c0d00
Zachary Caldwell <mintyst.john@gmail.com>
2021-08-27 15:18:44 -0700

    Added DOM elements

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 // import functions
+const nounOneInput = document.getElementById('noun-1-input');
+const pluralNounInput = document.getElementById('plural-noun-1-input');
+const nounTwoInput = document.getElementById('noun-2-input');
+const verbOneInput = document.getElementById('verb-1-input');
+const verbTwoInput = document.getElementById('verb-2-input');
+const verbThreeInput = document.getElementById('verb-3-input');
+const greetingInput = document.getElementById('greeting');
 
 // reference needed DOM elements
 
